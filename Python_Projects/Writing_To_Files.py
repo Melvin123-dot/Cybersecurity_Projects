d91cff3a28ec317877777f055ac31237223f01b0
Melvin123-dot <124604804+Melvin123-dot@users.noreply.github.com>
2025-01-28 13:48:38 +0000

    Update Writing_To_Files.py

--- Python_Projects/Writing_To_Files.py
+++ Python_Projects/Writing_To_Files.py
@@ -1,8 +1,8 @@
-employee_file = open(r"C:\Users\Lenovo\Desktop\Folders\My_Basic_To_Advanced_Python_Projects\Employees.txt", "a")
+employee_file = open("") #Create a file or use an existing one if possible
 employee_file.write("")
 employee_file.close()
 
 
-employee_file = open(r"C:\Users\Lenovo\Desktop\Folders\My_Basic_To_Advanced_Python_Projects\Employees1.txt", "w")
+employee_file = open("") #Use the already created file to write to it
 employee_file.write("Rose - Customer Service")
 employee_file.close()
