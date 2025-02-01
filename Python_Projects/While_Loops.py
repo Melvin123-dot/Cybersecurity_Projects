c92396b6828c3a2770f9a0f9ee7faf02d18d4e32
Melvin123-dot <124604804+Melvin123-dot@users.noreply.github.com>
2025-02-01 21:48:54 +0000

    Delete Python_Projects/While_Loops.py

--- Python_Projects/While_Loops.py
+++
@@ -1,9 +0,0 @@
-#In the first line of  code the variable i is initialized with the value of 1
-#In the second line of code the while loop checks whether i is less than or equal to 10.
-#Inside the loop body the current value of i is printed using the "print(i)" statement.
-#Then i is incremented by 1 using i += 1.
-#The loop continues executing as long as "i <= 10" remains true. In this case, it will print numbers from 1 to 10 (inclusive)
-i = 1
-while i <= 10:
-    print(i)
-    i += 1
