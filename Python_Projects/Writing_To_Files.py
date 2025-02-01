cd49a8704b7f04aa245cf997c9fe90eb21d258d3
Melvin123-dot <124604804+Melvin123-dot@users.noreply.github.com>
2025-02-01 21:49:29 +0000

    Delete Python_Projects/Writing_To_Files.py

--- Python_Projects/Writing_To_Files.py
+++
@@ -1,8 +0,0 @@
-employee_file = open("") #Create a file or use an existing one if possible
-employee_file.write("")
-employee_file.close()
-
-
-employee_file = open("") #Use the already created file to write to it
-employee_file.write("Rose - Customer Service")
-employee_file.close()
