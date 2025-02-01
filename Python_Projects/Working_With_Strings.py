b876fbbc44969640c731e8286e28d0ff4f0162fc
Melvin123-dot <124604804+Melvin123-dot@users.noreply.github.com>
2025-02-01 21:49:17 +0000

    Delete Python_Projects/Working_With_Strings.py

--- Python_Projects/Working_With_Strings.py
+++
@@ -1,9 +0,0 @@
-phrase = "Giraffe Academy"
-print(phrase.upper()) #This line of code uses the method ".upper" to change the original phrase to an upper case sentence
-print(phrase.lower()) #This line of code uses the method ".lower" to change the original phrase to a lower case sentence
-print(phrase.upper().isupper()) #This line of code uses methods such as the ".upper" and ".isupper" where the code checks by first converting the phrase
-#to an upper case using the ."upper" and then checks if the phrase is of course changed to upper case using the ".isupper" method to check if it's True.
-print(len(phrase)) #This line of code uses the method "len" to print out the length of the phrase
-print(phrase[0]) #This line of code prints the index of the phrase which happens to be the letter "G"
-print(phrase.index("G")) #This line of code will print the index of the letter "G" which is 0
-print(phrase.replace("Giraffe", "Dinasaur")) #This line of code uses the ".replace" method to replace the phrase "Giraffe" to "Dinasaur".
